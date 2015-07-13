b9d29aee0856f053c10a793b295c76ea5c1d53c2
jitterboggle <jeffreybartels@gmail.com>
2015-07-13 11:55:49 +1000

    Finished, albeit with bugs.

--- js/sketch.js
+++ js/sketch.js
@@ -1,20 +1,27 @@
 $(document).ready(function() {
-	var input = 64;
+	var input = 16;
 	Populate(input);
 	React();
 
+	//When the 'reset' button is clicked, all 'box' divs are removed, and the 'container'
+	//is then repopulated with 'Populate()' and the 'React()' function is called.
+
 	$('#reset').click(function(){
 		$('.box').remove();
-		//Call Functions to re-fill our container
 		Populate(input);
 		React();
 	});
 
+	//When the 'choose' button is clicked, all 'box-top' divs are removed, the user
+	//is then prompted to make a selection between 1 and 64. 
+	//The container is then repopulated with 'Populate()' function using the users
+	//input and the 'React()' function is called.
+
 	$('#choose').click(function(){
 		$('.box-top').remove();
-		input = parseInt(prompt('Please enter a number between 1 and 100'));
-		if (input > 100) {
-			input = 100;
+		input = parseInt(prompt('Please enter a number between 1 and 64'));
+		if (input > 64) {
+			input = 64;
 		}
 		else if (input < 1) {
 			input = 1;
@@ -25,30 +32,91 @@ $(document).ready(function() {
 		}
 	});
 
+	//When the 'random' button is clicked, all 'box-top' divs are removed, the user
+	//is then prompted to make a selection between 1 and 64. 
+	//The container is then repopulated with 'Populate()' function using the users
+	//input and the 'Colour()' function is called.
+
+	$('#random').click(function() {
+		$('box-top').remove();
+		input = parseInt(prompt('Please enter a number between 1 and 64'));
+		if (input > 64) {
+			input = 64;
+		}
+		else if (input < 1) {
+			input = 1;
+		}
+		else {
+			Populate(input);
+			Colour();
+		}
+	})
+
+	//When the 'opacity' button is clicked, all 'box-top' divs are removed, the user
+	//is then prompted to make a selection between 1 and 64. 
+	//The container is then repopulated with 'Populate()' function using the users
+	//input and the 'Shade()' function is called.
+
+	$('#opacity').click(function() {
+		$('box-top').remove();
+		input = parseInt(prompt('Please enter a number between 1 and 64'));
+		if (input > 64) {
+			input = 64;
+		}
+		else if (input < 1) {
+			input = 1;
+		}
+		else {
+			Populate(input);
+			Shade();
+		}
+	})
+
 });
 
+/*
+
+============Functions============
+
+ */
+
+// for loop appends divs with class 'box' to the 'container' div.
+// Individual 'box' divs' width and heights then adjusted to fit the fixed width
+// and height of the 'container' div
 
 function Populate(boxCount){
-	//For loop fills the grid
 	for (var i = 0; i<(boxCount*boxCount); i++) {
 		$('<div/>', {'class': 'box',}).appendTo('#container');
 	}
-	//Adjust each .box size to fit into the grid (960px)
 	var boxSize = 960 / boxCount;	
 	$('.box').css({'width' : boxSize,'height': boxSize});
 }
 
+// This function changes the class of the 'box' div to 'box-top' when a mouse 
+// hovers over it
+
 function React(){
 	$('.box').mouseenter(function(){	
 		$(this).addClass('box-top');
-		$(this).mouseleave(function(){
-			$(this).fadeTo('fast', 0.7);			
-		});
 	});
 }
 
-/*Heavily plagiarised from: 
+// This function changes the background-color of the 'box' div to a random colour
+// when a mouse hovers over it.
 
-https://github.com/muzfuz/CodeLessons/blob/master/Sketchpad/script.js
+function Colour () {
+	$('.box').mouseenter(function() {
+		$(this).css('background-color', 'rgb('+(Math.floor(Math.random() *256))+','
+			+(Math.floor(Math.random() *256))+','+(Math.floor(Math.random() *256))+')');
+	});
+}
 
-*/
+// This function reduces the opacity of any particular 'box' div each time a mouse
+// hovers over it.
+
+function Shade() {
+	$('.box').mouseenter(function() {
+		$(this).css('opacity', $(this).css('opacity') - 0.1);
+});
+}
+
